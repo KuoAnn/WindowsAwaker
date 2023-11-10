c9c9271bf926e7f0a68f69c0072c65dafa0fda0b
KuoAnn <b54744040@gmail.com>
2023-11-10 14:40:35 +0800

    Add application icon and keep system awake functionality

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,40 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            ButtonSwitch = new Button();
+            SuspendLayout();
+            // 
+            // ButtonSwitch
+            // 
+            ButtonSwitch.Dock = DockStyle.Fill;
+            ButtonSwitch.Location = new Point(0, 0);
+            ButtonSwitch.Margin = new Padding(2);
+            ButtonSwitch.Name = "ButtonSwitch";
+            ButtonSwitch.Size = new Size(171, 39);
+            ButtonSwitch.TabIndex = 0;
+            ButtonSwitch.Text = "Off";
+            ButtonSwitch.UseVisualStyleBackColor = true;
+            ButtonSwitch.Click += ButtonSwitch_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(171, 39);
+            Controls.Add(ButtonSwitch);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Margin = new Padding(2);
+            MaximizeBox = false;
+            Name = "Form1";
+            ShowInTaskbar = false;
+            Text = "Awaker";
+            TopMost = true;
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Button ButtonSwitch;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,114 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
 namespace Awaker
 {
     public partial class Form1 : Form
     {
+        private static bool IsKeepAwake = false;
+        private static string ModeText = string.Empty;
+
+        // 禁用休眠模式
+        [DllImport("Kernel32.dll", SetLastError = true)]
+        private static extern uint SetThreadExecutionState(EXECUTION_STATE state);
+
         public Form1()
         {
             InitializeComponent();
+
+            this.FormClosing += (obj, e) => UseNormalMode();
+
+            UseAwakeMode();
+            DestroyCountdown();
         }
+
+        private Icon GetEmbeddedIcon(string iconName)
+        {
+            // 取得內嵌資源
+            var assembly = Assembly.GetExecutingAssembly();
+            var iconStream = assembly.GetManifestResourceStream(iconName);
+
+            if (iconStream != null)
+            {
+                return new Icon(iconStream);
+            }
+
+            return SystemIcons.Exclamation;
+        }
+
+        public uint UseNormalMode()
+        {
+            IsKeepAwake = false;
+            ButtonSwitch.BackColor = Color.OrangeRed;
+            ModeText = "Normal Mode";
+            ButtonSwitch.Text = ModeText;
+
+            this.Icon = GetEmbeddedIcon("Awaker.icon.moon.ico");
+
+            return SetThreadExecutionState(EXECUTION_STATE.ES_DISPLAY_REQUIRED |
+                EXECUTION_STATE.ES_SYSTEM_REQUIRED |
+                EXECUTION_STATE.ES_CONTINUOUS);
+        }
+
+        public uint UseAwakeMode()
+        {
+            IsKeepAwake = true;
+            ButtonSwitch.BackColor = Color.GreenYellow;
+            ModeText = "Awake Mode";
+            ButtonSwitch.Text = ModeText;
+
+            this.Icon = GetEmbeddedIcon("Awaker.icon.sun.ico");
+
+            return SetThreadExecutionState(EXECUTION_STATE.ES_CONTINUOUS);
+        }
+
+        private System.Timers.Timer DestroyCountdown()
+        {
+            DateTime now = DateTime.Now;
+            // 計算距離 18:00 的時間間隔
+            DateTime targetTime = new(now.Year, now.Month, now.Day, 18, 0, 0);
+            TimeSpan timeRemaining = targetTime - now;
+            var timer = new System.Timers.Timer(timeRemaining.TotalMilliseconds);
+            timer.Elapsed += (sender, e) =>
+            {
+                UseNormalMode();
+
+                if (sender != null)
+                {
+                    ((System.Timers.Timer)sender).Stop();
+                }
+
+                Environment.Exit(0);
+            };
+
+            timer.Start();
+            return timer;
+        }
+
+        private void ButtonSwitch_Click(object sender, EventArgs e)
+        {
+            if (IsKeepAwake)
+            {
+                UseNormalMode();
+            }
+            else
+            {
+                DestroyCountdown();
+                UseAwakeMode();
+            }
+        }
+    }
+
+    // 定義執行狀態
+    [FlagsAttribute]
+    public enum EXECUTION_STATE : uint
+    {
+        NONE = 0,
+        ES_AWAYMODE_REQUIRED = 0x00000040,
+        ES_CONTINUOUS = 0x80000000,
+        ES_DISPLAY_REQUIRED = 0x00000002,
+        ES_SYSTEM_REQUIRED = 0x00000001
+        // Legacy flag, should not be used.
+        // ES_USER_PRESENT = 0x00000004
     }
 }
